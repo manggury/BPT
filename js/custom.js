a71afd79eeabe1a5447da1a7ba3d9910fc324a98
manggury <148739312+manggury@users.noreply.github.com>
2023-12-07 18:17:47 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -28,7 +28,21 @@ $(function () {
 
         $('.History_wrap .top_dots li').removeClass('on')
         $('.History_wrap .top_dots li').eq(idx).addClass('on')
-
     });
 
+    $('.news_wrap .left_tap .tab_tit li a').on('click', function (e) {
+        //1. a를 클릭햇을떄 새로고침 되지않게 하기 = function에 (e)
+        e.preventDefault();
+        //2. 번호를 받아오기
+        let idx = $(this).parent().index();
+        //3. 번호에 부합하는 tab_con>li를 보여주기
+        $('.news_wrap .left_tap .tab_con>li').removeClass('on');
+        $('.news_wrap .left_tap .tab_con>li').eq(idx).addClass('on');
+        //4. 메뉴에 스타일 붙이기
+        $('.news_wrap .left_tap .tab_tit>li').removeClass('on')
+        $('.news_wrap .left_tap .tab_tit>li').eq(idx).addClass('on')
+    });
+    $('.popup_slide').slick({
+        arrows: true,
+    });
 })
